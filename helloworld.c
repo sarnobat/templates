4e30f649ee248f8716c8e897e9431a5940500529
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2021-12-30 22:58:59 +0000

    Minor changes (untested)

--- helloworld.c
+++ helloworld.c
@@ -5,20 +5,14 @@
 // also see https://github.com/sarnobat/c_helloworld/tree/master/1_hello_world 
 int main(void)
 {
-	FILE *stream;
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
- 
-// 	stream = fopen("file.txt", "r");
-// 	if (stream == NULL)
-// 		exit(EXIT_FAILURE);
  
 	while ((read = getline(&line, &len, stdin)) != -1) {
 		printf("%s", line);
 	}
  
 	free(line);
-// 	fclose(stream);
 	exit(EXIT_SUCCESS);
 }
