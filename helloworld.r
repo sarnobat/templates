5acfe4d622d0f52182d8aff02ad98c2099aff6d9
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2021-12-30 18:43:30 +0000

    Minor changes (untested)

--- helloworld.r
+++ helloworld.r
@@ -1,1 +1,11 @@
-lines <- readLines("file.txt")
+#!/usr/bin/Rscript
+
+# this one might be useful too: https://www.rosettacode.org/wiki/Read_a_file_line_by_line#R
+
+d <- as.matrix(read.csv2(file("stdin"), header=FALSE, sep=","))
+ca <- commandArgs(trailingOnly=TRUE)
+pdf(file=ca[1], width=12, height=6)
+data <- d[,1:2]
+plot(data, type='n')
+lines(d)
+dev.off()
