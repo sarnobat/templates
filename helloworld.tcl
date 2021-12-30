de66310eeb73484479007f508a42651ee165cee2
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2021-12-30 01:34:07 +0000

    Minor changes (untested)

--- helloworld.tcl
+++ helloworld.tcl
@@ -1,9 +1,27 @@
-  set n 0
-  while {$n<3000} {
-    gets stdin row
-    puts $row
-    if { [string equal $row "."] } { break };
-    append message $row "\n"
-  }
-  puts "---------"
-  puts $message
+# set f [open stdin]
+while {[gets stdin line] >= 0} {
+	if { [file exists "/tmp"] } {
+		puts "$line exists"
+	} else {
+		# This loops over every line
+		puts ">>$line<<"
+    }
+}
+close $f
+
+#   while {[gets stdin line] >= 0} {
+# 	  puts $line
+#   }
+
+#   set n 0
+#   while {$n<30} {
+#     gets stdin row
+#     puts $row
+# #     if { [file exists "input.txt"] } {
+# # 		puts "input.txt exists"
+# # 	} 
+# #     if { [string equal $row "."] } { break };
+# #     append message $row "\n"
+#   }
+#   puts "---------"
+# #   puts $message
