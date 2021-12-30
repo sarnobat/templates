7150f3e0ab45746341b346709aa629e8e6a6462d
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2021-12-30 18:45:09 +0000

    Minor changes (untested)

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
  
+ /**
+ * find | /usr/local/Cellar/openjdk@11/11.0.12/bin/java helloworld.java
+ */
 public class HelloWorld {
     public static void main(String args[]) {
 	   try{
