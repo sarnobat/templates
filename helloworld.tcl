db5e4e3ad1c8cfadc8e8a0b0e1388642adbd7e94
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2021-12-30 01:36:56 +0000

    Minor changes (untested)

--- helloworld.tcl
+++ helloworld.tcl
@@ -1,27 +1,8 @@
-# set f [open stdin]
+# On wish, we get an exception. On tclsh we don't.
 while {[gets stdin line] >= 0} {
-	if { [file exists "/tmp"] } {
+	if { [file exists "$line"] } {
 		puts "$line exists"
 	} else {
-		# This loops over every line
 		puts ">>$line<<"
     }
 }
-close $f
-
-#   while {[gets stdin line] >= 0} {
-# 	  puts $line
-#   }
-
-#   set n 0
-#   while {$n<30} {
-#     gets stdin row
-#     puts $row
-# #     if { [file exists "input.txt"] } {
-# # 		puts "input.txt exists"
-# # 	} 
-# #     if { [string equal $row "."] } { break };
-# #     append message $row "\n"
-#   }
-#   puts "---------"
-# #   puts $message
