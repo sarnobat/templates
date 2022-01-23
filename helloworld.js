83b4c8ad6cfd8c00066d923b710a88232640a267
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2022-01-23 13:00:17 -0800

    Minor changes (untested)

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,13 @@
 //-----------------------------------------------------------------------------------------
 // EXAMPLE
 //
-//	find ~/trash/ | nodejs helloworld.js
+//	find ~/trash/	| node helloworld.js
 //
-// 	cat ~/.zshrc | nodejs helloworld.js
+// 	cat ~/.zshrc	| node helloworld.js
 //
+//
+//	npm install command-line-args --save
+
 // DATE
 //
 //	2022 Jan 23
@@ -17,12 +20,15 @@
 
 
 var readline = require('readline');
+//const commandLineArgs = require('command-line-args')
+
+
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
+	input: process.stdin,
+	output: process.stdout,
+	terminal: false
 });
 
 rl.on('line', function(line){
-    console.log('added: ' + line);
+	console.log('added: ' + line);
 })
