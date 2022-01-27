deea5c873e382636292db17cf3fb3d6010437900
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2022-01-26 22:04:29 -0800

    Minor changes (untested)

--- helloworld.py
+++ helloworld.py
@@ -32,7 +32,30 @@ from pathlib import Path
 # TODO: wrap this in a main function like the golang one, to show how to declare and call functions
 
 counts = {}
-
+sizes = {
+	100: 0,
+	1000 : 0,
+	10000 : 0,
+	100000 : 0,
+	1000000 : 0,
+	10000000 : 0,
+	100000000 : 0,
+	1000000000 : 0,
+	10000000000 : 0,
+	100000000000 : 0
+}
+counts_by_size = {
+	100: 0,
+	1000 : 0,
+	10000 : 0,
+	100000 : 0,
+	1000000 : 0,
+	10000000 : 0,
+	100000000 : 0,
+	1000000000 : 0,
+	10000000000 : 0,
+	100000000000 : 0
+}
 ###
 ### 5) CLI options
 ###
@@ -44,6 +67,7 @@ args = parser.parse_args()
 ###
 ### 1) Loop over stdin
 ###
+totalSize = 0
 input_stream = io.TextIOWrapper(sys.stdin.buffer, newline=None, encoding='latin1')
 for line in input_stream:
 
@@ -60,6 +84,39 @@ for line in input_stream:
 		# get absolute path
 		parent = Path(line).parent.absolute()
 		print (parent)
+		print("[DEBUG] exists: " + fullpath, file=sys.stderr)
+#		if Path(line).exists():
+		if os.path.isfile(fullpath):
+			size = Path(fullpath).stat().st_size
+			if size < 100:
+				sizes[100] += size
+				counts_by_size[100] += 1
+			elif size < 1000:
+				sizes[1000] += size
+				counts_by_size[1000] += 1
+			elif size < 10000:
+				sizes[10000] += size
+				counts_by_size[10000] += 1
+			elif size < 100000:
+				sizes[100000] += size
+				counts_by_size[100000] += 1
+			elif size < 1000000:
+				sizes[1000000] += size
+				counts_by_size[1000000] += 1
+			elif size < 100000000:
+				sizes[100000000] += size
+				counts_by_size[10000000] += 1
+			elif size < 1000000000:
+				sizes[1000000000] += size
+				counts_by_size[1000000000] += 1
+			elif size < 10000000000:
+				sizes[10000000000] += size
+				counts_by_size[10000000000] += 1
+			elif size < 100000000000:
+				sizes[100000000000] += size
+				counts_by_size[100000000000] += 1
+			totalSize += size
+			print ("[DEBUG] totalSize = " + str(totalSize), file=sys.stderr)
 
 
 		##
@@ -86,6 +143,7 @@ for line in input_stream:
 			print (line.strip(), end="\n")
 
 	else:
+		print("[DEBUG] not existing: " + fullpath, file=sys.stderr)
 		###
 		### 1) Print to stdout
 		###
@@ -96,3 +154,24 @@ for line in input_stream:
 # 		tokens = re.split(r'\.', line)
 # 		last_elem = tokens[len(tokens)-1]
 #		print (last_elem, end='')
+
+
+def sizeof_fmt(num, suffix=""):
+    for unit in ["", "k", "M", "G", "T", "P", "E", "Z"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
+
+print("100000000000 size:\t" 	+ sizeof_fmt(sizes[100000000000])	+ "\t(" + str(counts_by_size[100000000000]) + " files)")
+print("10000000000 size:\t" 	+ sizeof_fmt(sizes[10000000000]) 	+ "\t(" + str(counts_by_size[10000000000]) + " files)")
+print("1000000000 size:\t" 		+ sizeof_fmt(sizes[1000000000]) 	+ "\t(" + str(counts_by_size[1000000000]) + " files)")
+print("100000000 size:\t\t" 	+ sizeof_fmt(sizes[100000000]) 		+ "\t(" + str(counts_by_size[100000000]) + " files)")
+print("10000000 size:\t\t"	 	+ sizeof_fmt(sizes[10000000]) 		+ "\t(" + str(counts_by_size[10000000]) + " files)")
+print("1000000 size:\t\t"	 	+ sizeof_fmt(sizes[1000000]) 		+ "\t(" + str(counts_by_size[1000000]) + " files)")
+print("100000 size:\t\t"	 	+ sizeof_fmt(sizes[100000]) 		+ "\t(" + str(counts_by_size[100000]) + " files)")
+print("10000 size:\t\t"		 	+ sizeof_fmt(sizes[10000]) 			+ "\t(" + str(counts_by_size[10000]) + " files)")
+print("1000 size:\t\t"		 	+ sizeof_fmt(sizes[1000]) 			+ "\t(" + str(counts_by_size[1000]) + " files)")
+print("100 size:\t\t" 			+ sizeof_fmt(sizes[100]) 			+ "\t(" + str(counts_by_size[100]) + " files)")
+print("Total size: " + sizeof_fmt(totalSize))
