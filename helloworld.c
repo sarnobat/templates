76cc3b91cd4a9f324b451d23d8ec156349a9c9f1
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2022-03-07 22:12:27 -0800

    Minor changes (untested)

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 // gcc -c helloworld.c && gcc -o helloworldc helloworld.o && find | ./helloworldc stdin
-
 // also see https://github.com/sarnobat/c_helloworld/tree/master/1_hello_world 
+
+// I can't find a library that requires you to explicitly specify the path to it. glibc seems
+// to get found from some well-known place
 int main(void)
 {
 	char *line = NULL;
--- helloworld2.c
+++ helloworld2.c
@@ -1,8 +1,10 @@
+#include <assert.h>
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 // gcc -c helloworld2.c && gcc -o helloworld2c helloworld2c.o && find | ./helloworld2c stdin
-
+// I can't find a library that requires you to explicitly specify the path to it. glibc seems
+// to get found from some well-known place
 int main()
 {
 	char *line = NULL;
@@ -12,7 +14,10 @@ int main()
 	while ((read = getline(&line, &len, stdin)) != -1) {
 		printf("from c: %s", line);
 		regex_t re;
-		int i = regcomp(&re, "hello([a-bA-Z0-9]*)", REG_EXTENDED|REG_NOSUB);
+		int i = regcomp(&re, "hello([a-bA-Z0-9]*)", REG_EXTENDED|REG_NOSUB);		
+		assert (i == 0);
+		
+		 i = regexec(&re, "hello42world",0,NULL,0);
 	}
  
 	free(line);
