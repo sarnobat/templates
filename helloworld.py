1d9e43ae687ce0e66d4a1ce2fb0833c38c83db72
Sridhar Sarnobat <ss401533@gmail.com>
2022-08-02 18:32:44 -0700

    Minor changes (untested)

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
 import fileinput
-for line in fileinput.input():
-	print "[info] " + line.strip()
+import sys
+
+file = None;
+
+if (len(sys.argv) == 1):
+      file = sys.stdin
+else:
+      file =  open (sys.argv[-1], "r") # command-line argument
+
+for line in file:
+	print ("[info] " + line.strip())
