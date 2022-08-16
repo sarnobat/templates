272aef7cbadf7d3345640462d0ea4db36675969b
Sridhar Sarnobat <ss401533@gmail.com>
2022-08-15 22:50:17 -0700

    Minor changes (untested)

--- helloworld.java
+++ helloworld.java
@@ -16,11 +16,34 @@ import java.util.stream.Collectors;
  */
 public class Main {
 	public static void main(String args[]) {
+
+		// 4) cli options
+		if (System.getProperties().containsKey("-h")) {
+			System.out.println(help());
+			System.exit(-1);
+			throw new RuntimeException("so java compiles");
+		}
+		// 5) map/dictionary/associative array
+
+		// 7) convert epoch to date and vv
+		// 8) concurrent
 		try (BufferedReader bufferedReader = read(args, System.in)) {
 
+			// 1) stdin loop (with optional file arg)
 			for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
 				System.out.println(line);
+
+				// 2) regex capture groups
+				// 3) parse file path
+
+				// 10) web scrape
+				// 6) embed shell code inside high level language connecting pipes
+				// 9) print with padding
+				// 10) write to file
+
 			}
+
+			// 10) print current date
 
 		} catch (IOException e) {
 			System.err.println("Error: Target File Cannot Be Read");
@@ -36,12 +59,13 @@ public class Main {
 			// problem: arg files get ignored
 		} else {
 			if (args.length == 0) {
-				System.out.println("usage: helloworld [ARGS]");
+				System.out.println(help());
 				System.exit(-1);
 				throw new RuntimeException("so java compiles");
 			} else {
 				SequenceInputStream is = new SequenceInputStream(Collections.enumeration(Arrays.stream(args).map(f -> {
 					try {
+						// 1) file read
 						return new FileInputStream(f);
 					} catch (FileNotFoundException e) {
 						throw new RuntimeException(e);
@@ -51,5 +75,10 @@ public class Main {
 			}
 		}
 		return br;
+	}
+
+	private static String help() {
+		String help = "usage: helloworld [ARGS]";
+		return help;
 	}
 }
