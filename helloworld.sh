f0e5eb98dddfcb7216776adcb84d834fcad021b6
Sridhar Sarnobat <ss401533@gmail.com>
2022-08-22 03:06:52 -0700

    From Atom

--- helloworld.sh
+++ helloworld.sh
@@ -15,7 +15,7 @@ while getopts ":ht" opt; do
 done
 
 while read line ; do
-  
+
 	if [[ $line =~ ^\/[a-zA-Z0-9]* ]]; then
 		echo "(is absolute, removing CWD)" >&2
 		line_after=${line#$PWD}
@@ -32,7 +32,7 @@ while read line ; do
 	# 	line_after=${line//o/PREFIX}
 	# Looks like ^ isn't supported in variable expansion
 
-  
+
 done < "${1:-/dev/stdin}"
 
 
@@ -56,4 +56,3 @@ curl 'https://www.google.com' | lynx  -stdin -image_links  -dump
 ### 2D plot
 seq 1 1000| shuf | xargs -n 2 | sort -n | tee /tmp/hello.dat
 gnuplot -e "set terminal dumb; plot '/tmp/hello.dat'"
-
