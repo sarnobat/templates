db0e5456d6a725f44aba50f70ae79f2f48494e82
Sridhar Sarnobat <srsarnob@cisco.com>
2022-11-03 18:21:38 -0700

    Minor changes (untested)

--- helloworld.multiargs.sh
+++ helloworld.multiargs.sh
@@ -4,11 +4,9 @@
 # https://unix.stackexchange.com/questions/137566/arrays-in-unix-bourne-shell
 ARGS="$@"
 
-# TODO: really we need a temp file
-# rm /tmp/args.txt
-
 ARGS_TXT=`mktemp`
-trap 'echo dirty end $ARGS_TXT' SIGINT
+trap 'echo dirty end (local) $ARGS_TXT' SIGINT
+trap 'echo dirty end (pipe)  $ARGS_TXT' SIGPIPE
 
 # args are space-delimited
 for VALUE in $ARGS; do
@@ -16,8 +14,7 @@ for VALUE in $ARGS; do
 done
 
 # stdin is newline-delimited
-cat $ARGS_TXT /dev/stdin | while read line ; do
+cat /dev/stdin | cat - $ARGS_TXT  | while read line ; do
 	echo $line
 done
 echo "clean end: $ARGS_TXT"
-# rm /t$mp/args.txt
