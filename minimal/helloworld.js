b70de4443920dee1b328daa0fc41c4f715c424ec
Sridhar Sarnobat <srsarnob@cisco.com>
2023-01-27 20:46:53 -0800

    Minor changes (untested)

--- minimal/helloworld.js
+++ minimal/helloworld.js
@@ -1,12 +1,7 @@
 //-----------------------------------------------------------------------------------------
 // EXAMPLE
 //
-//	find ~/trash/	| node helloworld.js -n 4
-//
-// 	cat ~/.zshrc	| node helloworld.js
-//
-//
-//	npm install command-line-args --save
+// 	node helloworld.js
 //
 // DATE
 //
@@ -18,53 +13,5 @@
 //
 //-----------------------------------------------------------------------------------------
 
-const readline		= require('readline');
+console.log('added: ');
 
-///
-/// 5) CLI options
-///
-
-var stdio = require('stdio');
-var getopt = stdio.getopt;
-
-
-const options = getopt({
-	num_max: {key: 'n', default : 5, description: 'If the project is finished'}
-});
-
-
-const optionDefinitions = [
-  {
-    name: 'help',
-    alias: 'h',
-    type: Boolean,
-    description: 'Display this usage guide.'
-  },
-  { name: 'verbose', alias: 'v', type: Boolean },
-  { name: 'src', type: String, multiple: true, defaultOption: true },
-  { name: 'num-max', alias: 'n', type: Number, defaultValue : 5}
-]
-//const options2 = commandLineArgs(optionDefinitions)
-
-
-
-	///
-	/// 1) Loop over stdin
-	///
-var rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-	terminal: false
-});
-
-
-
-rl.on('line', function(line){
-//	console.log(options2.n);
-//	console.log(ops.kaka);
-	console.log(options);
-	console.log('added: ' + line);
-//	if (ops.kaka && ops.check) {
-//	    console.log(ops.kaka + ops.check[0]);
-//	}
-})
