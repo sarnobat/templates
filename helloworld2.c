8942dfe0e0e95569e81d5a6ff6ef0c1bbc369d18
Sridhar Sarnobat <ss401533@gmail.com>
2023-02-01 23:25:05 -0800

    Minor changes (untested)

--- helloworld2.c
+++ helloworld2.c
@@ -1,10 +1,18 @@
+// gcc -c helloworld2.c -o helloworld2c.o && gcc -o helloworld2c helloworld2c.o && find | ./helloworld2c
+
+// also see https://github.com/sarnobat/c_helloworld/tree/master/1_hello_world 
+
+// I can't find a library that requires you to explicitly specify the path to it. glibc seems
+// to get found from some well-known place
+
+// Another example that works:
+// /Volumes/git/github/c_helloworld/6_exif_library
+
 #include <assert.h>
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
-// gcc -c helloworld2.c && gcc -o helloworld2c helloworld2c.o && find | ./helloworld2c stdin
-// I can't find a library that requires you to explicitly specify the path to it. glibc seems
-// to get found from some well-known place
+
 int main()
 {
 	char *line = NULL;
@@ -12,12 +20,22 @@ int main()
 	ssize_t read;
  
 	while ((read = getline(&line, &len, stdin)) != -1) {
-		printf("from c: %s", line);
-		regex_t re;
-		int i = regcomp(&re, "hello([a-bA-Z0-9]*)", REG_EXTENDED|REG_NOSUB);		
-		assert (i == 0);
 		
-		 i = regexec(&re, "hello42world",0,NULL,0);
+		fprintf( stderr, "[debug] %s", line);
+		
+		regex_t re;
+		int compiled = regcomp(&re, "hello([a-bA-Z0-9]*)", REG_EXTENDED|REG_NOSUB);		
+		assert (compiled == 0);
+
+		int matchFound = regexec(&re, line, 0, NULL, 0);
+// 		printf("%s\n", compared);
+		if  (matchFound == 0) {
+			printf("match: %s", line);
+		} else if (matchFound == REG_NOMATCH) {
+//			printf("no match: %s", line);
+		} else {
+			printf("error: %s", line);
+		}
 	}
  
 	free(line);
