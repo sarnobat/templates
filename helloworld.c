c62e9f835051491ddf88cf03deeb481a7f65076f
Sridhar Sarnobat <ss401533@gmail.com>
2023-02-05 20:50:11 -0800

    Minor changes (untested)

--- helloworld.c
+++ helloworld.c
@@ -19,8 +19,12 @@ int main()
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
+	size_t maxMatches = 2;
 	size_t maxGroups = 3;
 	regmatch_t groupArray[maxGroups];
+	unsigned int m = 0;
+	char * cursor;
+
 
  	///
 	/// 1) Loop over stdin
@@ -36,31 +40,42 @@ int main()
 		int compiled = regcomp(&re, "hello([a-bA-Z0-9]*)", REG_EXTENDED|REG_NOSUB);		
 		assert (compiled == 0);
 
-		int matchFound = regexec(&re, line, 0, groupArray, 0);
-		if  (matchFound == 0) {
-			int g = 0;		
 
-			printf("match: %s", line);
+		m = 0;
+		cursor = line;
+		for (m = 0; m < maxMatches; m ++) {
+				int matchFound = regexec(&re, line, 0, groupArray, 0);
+				if  (matchFound == 0) {
+		// 			int g = 0;		
 
-			for (g = 0; g < maxGroups; g++) {
-				if (groupArray[g].rm_so == (size_t)-1) {
-					break;  // No more groups
+				// 	printf("match: %s", line);
+// 
+// 
+					unsigned int g = 0;
+					unsigned int offset = 0;
+// 					for (g = 0; g < maxGroups; g++) {
+// 						if (groupArray[g].rm_so == (size_t)-1) {
+// 							break;  // No more groups
+// 						}
+// 				
+// 						if (g == 0) {
+// 							offset = groupArray[g].rm_eo;
+// 						}
+// 
+// 						char cursorCopy[strlen(cursor) + 1];
+// 						strcpy(cursorCopy, cursor);
+// 						cursorCopy[groupArray[g].rm_eo] = 0;
+// 						printf("Match %u, Group %u: [%2u-%2u]: %s\n",
+// 							 m, g, groupArray[g].rm_so, groupArray[g].rm_eo,
+// 							 cursorCopy + groupArray[g].rm_so);
+// 					}
+				} else if (matchFound == REG_NOMATCH) {
+					fprintf( stderr, "[debug] no match: %s", line);
+				} else {
+					printf("error: %s", line);
 				}
-
-				char sourceCopy[strlen(line) + 1];
-				strcpy(sourceCopy, line);
-// 				sourceCopy[groupArray[g].rm_eo] = 0;
-// 				printf("Group %u: [%2ld-%2ld]: %s\n",
-// 					g, groupArray[g].rm_so, groupArray[g].rm_eo,
-// 					sourceCopy + groupArray[g].rm_so);
-			}
-		} else if (matchFound == REG_NOMATCH) {
-			fprintf( stderr, "[debug] no match: %s", line);
-		} else {
-			printf("error: %s", line);
 		}
-	}
- 
+ 	}
 	free(line);
 	exit(EXIT_SUCCESS);
 }
