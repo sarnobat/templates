f9e26c0108742adf3e7ec18bba7e86fe5d54967c
Sridhar Sarnobat <ss401533@gmail.com>
2023-02-05 21:01:20 -0800

    Minor changes (untested)

--- helloworld.c
+++ helloworld.c
@@ -12,92 +12,39 @@
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main()
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
-	size_t maxMatches = 2;
-	size_t maxGroups = 3;
-	regmatch_t groupArray[maxGroups];
-	unsigned int m = 0;
-	char * cursor;
-
+	regmatch_t substmatch[1];
 
  	///
 	/// 1) Loop over stdin
 	///
 	while ((read = getline(&line, &len, stdin)) != -1) {
-
-		///
-		/// 1) Print to stdout or stderr
-		///		
+		
 		fprintf( stderr, "[debug] line: %s", line);
 		
 		regex_t re;
 		int compiled = regcomp(&re, "hello([a-bA-Z0-9]*)", REG_EXTENDED|REG_NOSUB);		
 		assert (compiled == 0);
 
-
-		m = 0;
-		cursor = line;
-		for (m = 0; m < maxMatches; m ++) {
-			if (regexec(&regexCompiled, cursor, maxGroups, groupArray, 0))
-			break;  // No more matches
-
-			unsigned int g = 0;
-			unsigned int offset = 0;
-			for (g = 0; g < maxGroups; g++)
-			{
-			if (groupArray[g].rm_so == (size_t)-1)
-			break;  // No more groups
-
-			if (g == 0)
-			offset = groupArray[g].rm_eo;
-
-			char cursorCopy[strlen(cursor) + 1];
-			strcpy(cursorCopy, cursor);
-			cursorCopy[groupArray[g].rm_eo] = 0;
-			printf("Match %u, Group %u: [%2u-%2u]: %s\n",
-				 m, g, groupArray[g].rm_so, groupArray[g].rm_eo,
-				 cursorCopy + groupArray[g].rm_so);
-			}
-			cursor += offset;
-/*
-				int matchFound = regexec(&re, line, 0, groupArray, 0);
-				if  (matchFound == 0) {
-		// 			int g = 0;		
-
-				// 	printf("match: %s", line);
-// 
-// 
-					unsigned int g = 0;
-					unsigned int offset = 0;
-// 					for (g = 0; g < maxGroups; g++) {
-// 						if (groupArray[g].rm_so == (size_t)-1) {
-// 							break;  // No more groups
-// 						}
-// 				
-// 						if (g == 0) {
-// 							offset = groupArray[g].rm_eo;
-// 						}
-// 
-// 						char cursorCopy[strlen(cursor) + 1];
-// 						strcpy(cursorCopy, cursor);
-// 						cursorCopy[groupArray[g].rm_eo] = 0;
-// 						printf("Match %u, Group %u: [%2u-%2u]: %s\n",
-// 							 m, g, groupArray[g].rm_so, groupArray[g].rm_eo,
-// 							 cursorCopy + groupArray[g].rm_so);
-// 					}
-				} else if (matchFound == REG_NOMATCH) {
-					fprintf( stderr, "[debug] no match: %s", line);
-				} else {
-					printf("error: %s", line);
-				}*/
+		int matchFound = regexec(&re, line, 0, substmatch, 0);
+		if  (matchFound == 0) {
+		
+			///
+			/// 1) Print to stdout
+			///
+			printf("match: %s", line);
+		} else if (matchFound == REG_NOMATCH) {
+			fprintf( stderr, "[debug] no match: %s", line);
+		} else {
+			printf("error: %s", line);
 		}
- 	}
+	}
+ 
 	free(line);
 	exit(EXIT_SUCCESS);
 }
