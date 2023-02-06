f22feb65beb8602459ed04b5b0d28283de135cf8
Sridhar Sarnobat <ss401533@gmail.com>
2023-02-05 20:50:56 -0800

    Minor changes (untested)

--- helloworld.c
+++ helloworld.c
@@ -44,6 +44,28 @@ int main()
 		m = 0;
 		cursor = line;
 		for (m = 0; m < maxMatches; m ++) {
+			if (regexec(&regexCompiled, cursor, maxGroups, groupArray, 0))
+			break;  // No more matches
+
+			unsigned int g = 0;
+			unsigned int offset = 0;
+			for (g = 0; g < maxGroups; g++)
+			{
+			if (groupArray[g].rm_so == (size_t)-1)
+			break;  // No more groups
+
+			if (g == 0)
+			offset = groupArray[g].rm_eo;
+
+			char cursorCopy[strlen(cursor) + 1];
+			strcpy(cursorCopy, cursor);
+			cursorCopy[groupArray[g].rm_eo] = 0;
+			printf("Match %u, Group %u: [%2u-%2u]: %s\n",
+				 m, g, groupArray[g].rm_so, groupArray[g].rm_eo,
+				 cursorCopy + groupArray[g].rm_so);
+			}
+			cursor += offset;
+/*
 				int matchFound = regexec(&re, line, 0, groupArray, 0);
 				if  (matchFound == 0) {
 		// 			int g = 0;		
@@ -73,7 +95,7 @@ int main()
 					fprintf( stderr, "[debug] no match: %s", line);
 				} else {
 					printf("error: %s", line);
-				}
+				}*/
 		}
  	}
 	free(line);
