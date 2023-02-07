aa3fdd482e20e54db30e3928c859c374fa92cbba
Sridhar Sarnobat <ss401533@gmail.com>
2023-02-06 18:37:28 -0800

    Minor changes (untested)

--- helloworld.c
+++ helloworld.c
@@ -209,20 +209,14 @@ int main (int argc, char **argv) {
 	//	
 	FILE *fp;
 	char path[1035];
-
-	/* Open the command for reading. */
 	fp = popen("uname -a", "r");
 	if (fp == NULL) {
 		printf("Failed to run command\n" );
 		exit(1);
 	}
-
-	/* Read the output a line at a time - output it. */
 	while (fgets(path, sizeof(path), fp) != NULL) {
 		printf("[shell command] %s", path);
 	}
-
-	/* close */
 	pclose(fp);
 
 	//
