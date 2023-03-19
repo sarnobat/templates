c8ec029a569b399abcb071caf353c5d2cfaa401e
Sridhar Sarnobat <ss401533@gmail.com>
2023-03-19 00:01:21 -0700

    Minor changes (untested)

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,18 +1,11 @@
-//-----------------------------------------------------------------------------------------
-// EXAMPLE
-//
-// g++ helloword.cpp -o helloword
-//
-// DATE
-//
-//	2023 Mar 18
-//
-//-----------------------------------------------------------------------------------------
-
+// /Volumes/numerous/usr/local/homebrew/Cellar/gcc/11.3.0_2/bin/g++-11 -I/Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/include/ -L /Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/lib  -Wall -m64 helloworld.cpp -o helloworld
+#include "boost/filesystem.hpp"
+#include <string>
 #include <iostream>
 
-int main() {
 
+int main()
+{
 	///
 	/// 5) CLI options
 	///
