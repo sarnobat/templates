d9a0c921712a2a8e5481b5031d48d3e31061f13b
Sridhar Sarnobat <ss401533@gmail.com>
2023-03-19 00:16:28 -0700

    Minor changes (untested)

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,28 +1,29 @@
-// /Volumes/numerous/usr/local/homebrew/Cellar/gcc/11.3.0_2/bin/g++-11 -I/Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/include/ -L /Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/lib  -Wall -m64 helloworld.cpp -o helloworld
+// /Volumes/numerous/usr/local/homebrew/Cellar/gcc/11.3.0_2/bin/g++-11 -I/Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/include/ -L /Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/lib/ -l boost_filesystem helloworld.cpp -o helloworld
 #include "boost/filesystem.hpp"
 #include <string>
 #include <iostream>
-
 
 int main()
 {
 	///
 	/// 5) CLI options
 	///
+
 	using namespace std;
-
-
-
 	std::string line;
-	while (std::getline(std::cin, line)) {
-		boost::filesystem::path file(line);
-		std::cout << "line: " << line << std::endl;
-	}
-//	cout << "Hello, World" << endl;
-
-
 	///
 	/// 1) Loop over stdin
 	///
+
+	while (std::getline(std::cin, line)) {
+
+		boost::filesystem::path file(line);		
+		if (boost::filesystem::exists(line)) {
+			std::cout << "exists: " << line << std::endl;
+		}
+		else {
+			std::cout << "does not exist: " << line << std::endl;
+		}
+	}
 	return 0;
 }
