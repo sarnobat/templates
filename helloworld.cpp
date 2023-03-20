b9839fbd4d56a47b381e52978c4fc4e0812ae493
Sridhar Sarnobat <ss401533@gmail.com>
2023-03-19 22:18:46 -0700

    Minor changes (untested)

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,7 +5,7 @@
 //
 // COMPILE TO NATIVE
 //
-//		/Volumes/numerous/usr/local/homebrew/Cellar/gcc/11.3.0_2/bin/g++-11 -I/Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/include/ -L /Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/lib/ -l boost_filesystem helloworld.cpp -o helloworld
+//		/Volumes/numerous/usr/local/homebrew/Cellar/gcc/11.3.0_2/bin/g++-11 -I/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/ -I/Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/include/ -L /Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/lib/ -l boost_filesystem helloworld.cpp -o helloworld
 //
 // DATE
 //
@@ -17,6 +17,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <limits.h>
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
@@ -29,13 +31,20 @@ int main(int argc, char* argv[]) {
 		struct stat buffer;
 		if (stat (myString.c_str(), &buffer) == 0) {
 			//cout << "exists:\t" << myString << std::endl;
-			
+			// check if it's a file or a dir
 			if (S_ISREG(buffer.st_mode)) {
 				cout << "file:\t" << myString << std::endl;
 			} else if (S_ISDIR(buffer.st_mode)) {
 				cout << "dir:\t" << myString << std::endl;
 			}
+			// get absolute path
+			char resolved_path[PATH_MAX]; 
+			realpath(myString.c_str(), resolved_path);
+			cout << "abs:\t" << resolved_path << std::endl;
 		} else {
+			///
+			/// 1) Print to stdout
+			///
 			cout << "not existing:\t" << myString << std::endl;
 		}
 	}
