a784b94132348650a1566d971564c0feb70a1564
Sridhar Sarnobat <ss401533@gmail.com>
2023-03-19 22:03:56 -0700

    Minor changes (untested)

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,5 @@
 // /Volumes/numerous/usr/local/homebrew/Cellar/gcc/11.3.0_2/bin/g++-11 -I/Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/include/ -L /Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/lib/ -l boost_filesystem helloworld.cpp -o helloworld
-// #include <iostream>
-#include <boost/filesystem.hpp>
-// using namespace std;
-using namespace boost::filesystem;
+
 #include <sys/stat.h>
 #include <iostream>
 #include <fstream>
@@ -11,11 +8,12 @@ using namespace std;
 
 // string filename = "/tmp/";
 
-int main(int argc, char* argv[])
-{
-
+int main(int argc, char* argv[]) {
+	///
+	/// 1) Loop over stdin
+	///
 	std::string line;
-	while (std::getline(std::cin, line)) {		
+	while (std::getline(std::cin, line)) {
 		std::string myString = line;
 		struct stat buffer;
 		if (stat (myString.c_str(), &buffer) == 0) {
