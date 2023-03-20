c901e99ed83a2fe92f836439e749769d42f3fe9a
Sridhar Sarnobat <ss401533@gmail.com>
2023-03-19 22:02:18 -0700

    Minor changes (untested)

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,29 +1,34 @@
 // /Volumes/numerous/usr/local/homebrew/Cellar/gcc/11.3.0_2/bin/g++-11 -I/Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/include/ -L /Volumes/numerous/2022/usr/local/homebrew/Cellar/boost/1.81.0_1/lib/ -l boost_filesystem helloworld.cpp -o helloworld
-#include "boost/filesystem.hpp"
-#include <string>
+// #include <iostream>
+#include <boost/filesystem.hpp>
+// using namespace std;
+using namespace boost::filesystem;
+#include <sys/stat.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main()
+// string filename = "/tmp/";
+
+int main(int argc, char* argv[])
 {
-	///
-	/// 5) CLI options
-	///
 
-	using namespace std;
 	std::string line;
-	///
-	/// 1) Loop over stdin
-	///
-
-	while (std::getline(std::cin, line)) {
-
-		boost::filesystem::path file(line);		
-		if (boost::filesystem::exists(line)) {
-			std::cout << "exists: " << line << std::endl;
-		}
-		else {
-			std::cout << "does not exist: " << line << std::endl;
+	while (std::getline(std::cin, line)) {		
+		std::string myString = line;
+		struct stat buffer;
+		if (stat (myString.c_str(), &buffer) == 0) {
+			cout << "exists:\t" << myString << std::endl;
+		} else {
+			cout << "not existing:\t" << myString << std::endl;
 		}
 	}
+	//   cout << "exists: " << (stat (myString.c_str(), &buffer) == 0) << "\n";
+
+	//   std::ofstream o(filename.c_str());
+
+	//   o << "Hello, World\n" << std::endl;
+
 	return 0;
 }
