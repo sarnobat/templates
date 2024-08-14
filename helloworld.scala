d680abee6cb7945ae712ef8212d4430c7bdae8c7
Sridhar Sarnobat <sridhar.sarnobat@stanford.edu>
2024-08-14 15:53:51 -0700

    Minor changes (untested) 2024-08-14

--- helloworld.scala
+++ helloworld.scala
@@ -1,6 +1,8 @@
+// Looks like conversion to a native image can only be done inside an sbt project, not like go build.
+
 import scala.io.StdIn
 
-object ReadLines {
+object HelloWorld {
   def main(args: Array[String]): Unit = {
     var line: String = StdIn.readLine()
     while (line != null) {
